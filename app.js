e1d0701e064c835e9f5f13ea80e41a5696310cfc
Bassel-Bakr <basselbakr@gmail.com>
2018-03-31 21:21:30 +0200

    update dependencies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const path = require('path');
 const sass = require('node-sass-middleware');
+const express = require('express');
 const app = express();
 app.use(sass({
   src: __dirname,
@@ -7,6 +8,6 @@ app.use(sass({
   indentedSyntax: false
 }));
 app.use(express.static(__dirname));
-app.get("/", (req, res) => res.sendFile("website.html"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "website.html")));
 app.use((req, res) => res.send("404 :P"));
 app.listen(process.env.PORT || 5500);
